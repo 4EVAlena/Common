2f11023681084724cc5d63e1898c99eb677dabc7
EVA <evapavlova0000@gmail.com>
2020-03-21 17:35:38 +0300

    +

--- lesson24_2/js/lesson2422.js
+++ lesson24_2/js/lesson2422.js
@@ -14,7 +14,13 @@ document.addEventListener('DOMContentLoaded', () => {
         flex-direction: column;
         align-items: center;
         background: #000;
-    }    
+        color: grey; 
+    }  
+    .btnContainer {
+        margin: 40 auto;
+        flex-direction: row;
+        background: #000;
+    }
     .tablo {
         margin: 10px;
         padding: 10px;
@@ -29,52 +35,94 @@ document.addEventListener('DOMContentLoaded', () => {
     .card {
         margin: 10px;
         padding: 10px;
-        width: 150px;
-        height: 150px;
+        width: 200px;
+        height: 330px;
         color: white;
         background: black;
-        border: thin inset red;
+        text-align: center;
+        font-size: 16px;
     }    
-    .btnShowAll {
-        margin: 10px;
-        padding: 10px 20px;        
+    button {
+        padding: 10px 20px;  
+        color: blue;
+        font-size: 16px; 
+        font-weight: bold;             
+    }
+    img {
+        display: block; 
+        overflow: hidden;
+        margin: 0 auto;
+        max-width: 70%;
+        height: 30%;
     }
     `;
+    document.head.appendChild(style);
     // создаем и выводим на страницу первый контейнер-блок
     const divContainer1 = document.createElement('div');
-    divContainer1.className = 'container';
-    divContainer1.innerText = 'Вся Картотека';
+    divContainer1.classList.add('container');
+    divContainer1.innerText = 'Картотека Героев';
+    divContainer1.style.fontSize = "30px";
+    document.body.appendChild(divContainer1);
+
+    const btnContainer = document.createElement('div');
+    btnContainer.classList.add('btnContainer');
+    divContainer1.append(btnContainer);
+
+    const button = document.getElementById('btnShowAll');
+    btnContainer.append(button);
+    const button2 = document.createElement('select');
+    button2.classList.add('button');
+    btnContainer.append(button2);
+
+    let option = document.createElement('option');  // остановилась тут, в работе
+    option.classList.add('button');
+    option.value = "Выбери фильм";
+    button2.append(option);
      
     const tablo = document.createElement('div');
     tablo.className = 'tablo';
-
-    // const data = './dbHeroes.json';
-    // Получим/ распарсим данные из файла json и выведем их в карточки
-    const button = document.getElementById('btnShowAll')
+    tablo.style.display = "none";
+    divContainer1.append(tablo);
+    
     button.addEventListener('click', () => {
+        tablo.style.display = "";
         const request = new XMLHttpRequest();
-        request.open('GET', 'dbHeroes.json');
+        request.open('GET', './dbHeroes.json');
         request.setRequestHeader('Content-Type', 'application/json');
         request.send();
         request.addEventListener('readystatechange', (event) => {
-            if(request.readyState === 4 && request.status === 200) {
-                const cardsArray = JSON.parse(request.responseText);
-                cardsArray.forEach ((card) => {
-                    cardTablo = document.createElement('div');
-                    cardTablo.classList.add('card');
-                    const {photo, actors, realName, movies, status} = card;
-                    cardTablo.innerHTML = `${photo} <br>
-                                        Актер: ${actors} <br>
-                                        Настоящее имя: ${realName} <br>
-                                        Фильмы: ${movies} <br>
-                                        Статус: ${status}`;
-                    tablo.appendChild(cardTablo);            
-                });
-            } else {
+            if (request.readyState !== 4 && request.status !== 200) {
                 divContainer1.innerText = 'Произошла ошибка';
-            }  
-        });    
-    });    
+                return;
+            }
+            const cardsArray = JSON.parse(request.responseText);
+            cardsArray.forEach((card) => {
+              let cardTablo = document.createElement('div');
+              cardTablo.classList.add('card');
+              const {
+                photo,
+                actors,
+                realName,
+                movies,
+                status
+              } = card;
+              console.log(photo);
+              cardTablo.innerHTML = `<img src="${photo}"> <br>
+                                            Актер: ${actors} <br>
+                                            Настоящее имя: ${realName} <br>
+                                            Фильмы: ${movies} <br>
+                                            Статус: ${status}`;
+              tablo.appendChild(cardTablo);
+            });
+          });
+        });
 
-    divContainer1.append(tablo);
-}) 
+        button2.addEventListener('click', (event) => {
+
+        });
+
+
+
+
+});
+
